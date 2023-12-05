97f4ee4da1f71e30198ea3f81e0a36c1f0f5665a
dsantosgonzalez <santosdiegosd@gmail.com>
2023-12-04 22:20:23 -0500

    final final

--- Trie.cpp
+++ Trie.cpp
@@ -5,6 +5,7 @@
 #include "Trie.h"
 
 Node::Node() {
+    //dont use uninitialized vars!
     leaf = 0;
     for(auto& c: children)
         c = nullptr;
@@ -17,29 +18,28 @@ Trie::Trie() {
 void Trie::insert(std::string password) {
     Node* curr = root;
     for(auto& c: password){
-        //relative position
-        int i = c - ASCII_OFFSET;
+        int i = c - ASCII_OFFSET; //relative position
         if(!curr->children[i])
             curr->children[i] = new Node();
         curr = curr->children[i];
     }
-    curr->leaf = 1;
+    curr->leaf = 1; //if it's a leaf, it's a word
 }
 
 bool Trie::search(std::string password) {
     Node* curr = root;
     for(auto& c: password){
         int i = c - ASCII_OFFSET;
-        if(!curr->children[i])
+        if(!curr->children[i]) //not fount
             return 0;
         curr = curr->children[i];
     }
-    return curr->leaf;
+    return curr->leaf; //reached the end. it's found if this node is a leaf
 }
 
 bool Trie::allowed(std::string password) {
     for(char& c: password){
-        if(c<32 || c>126)
+        if(c<32 || c>126) //printable ASCII characters are in this range
             return 0;
     }
     return 1;
--- Trie.h
+++ Trie.h
@@ -8,11 +8,11 @@
 #include <iostream>
 #include <string>
 const int ALPHABET = 95; //this will be changed later. printable ASCII characters 32 to 126. 126-32+1 = 95
-const int ASCII_OFFSET = 32;
+const int ASCII_OFFSET = 32; //the range of ASCII characters we care about starts at 32
 
 
 struct Node{
-    Node* children[ALPHABET];
+    Node* children[ALPHABET]; //i know it's a little memory hungry but look at that lovely time complexity
     bool leaf;
 
     Node();
